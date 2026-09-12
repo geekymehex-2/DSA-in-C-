665fadef3c590cd811676daccbdf061fa3211924
Geeky-Nerd <geekymehex@gmail.com>
2026-09-12 20:08:48 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (98.66%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int n = arr.size();
+        int insertpos = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                arr[insertpos++] = arr[i];
+            }
+        }
+        for(int i = insertpos;i<n;i++ ){
+            arr[i] = 0;
+        }
+    }
+};
