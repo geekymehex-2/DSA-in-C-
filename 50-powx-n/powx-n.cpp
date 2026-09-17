41efa3fbf4d51d6e0c5ce6edd77f94a2c3a302ef
Geeky-Nerd <geekymehex@gmail.com>
2026-09-17 12:44:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (71.94%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if(N<0){
+            x = 1/x;
+             N = -N;
+        }
+        return solve(x,N);
+    }
+    double solve(double x,long long N){
+        if(N==0) return 1;
+        double half = solve(x,N/2);
+        if(N%2==0)  return half*half;
+        else return half*half*x;
+    }
+};
